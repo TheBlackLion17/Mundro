be7bc64dbb9cda152fe5d847e352807e7d89d093
AK karma <89985695+TheBlackLion17@users.noreply.github.com>
2026-03-12 18:57:59 +0530

    Update register.js

--- js/register.js
+++ js/register.js
@@ -2,6 +2,7 @@ const TOKEN = "8788345094:AAGOxd3V-80F5Th992cF69HNWUjMPg4OLa4";
 const CHAT_ID = "7705748477";
 
 const WHATSAPP_GROUP = "https://chat.whatsapp.com/FhLWWkQgmlXLK5Y5GstoeA?mode=gi_t";
+const DISCORD_SERVER = "https://discord.gg/229XR2wY";
 
 document.getElementById("registerForm").addEventListener("submit", function(e){
 
@@ -32,8 +33,17 @@ text:message
 });
 
 // Open WhatsApp group
+alert("✅ Registration Successful! Join WhatsApp & Discord.");
+
+// Open WhatsApp
 window.open(WHATSAPP_GROUP,"_blank");
 
-alert("Registration sent! Join WhatsApp group.");
+// Open Discord
+window.open(DISCORD_SERVER,"_blank");
+
+}catch(error){
+
+alert("❌ Error sending registration. Try again.");
+
 
 });
